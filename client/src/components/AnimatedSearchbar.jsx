941da1078583fee5cb55edcfdbf3163a18e08cc4
imUmer <imumerniaz@gmail.com>
2024-12-26 00:00:02 +0300

    search bar animation fixes

--- client/src/components/AnimatedSearchbar.jsx
+++ client/src/components/AnimatedSearchbar.jsx
@@ -26,15 +26,15 @@ const AnimatedSearchBar = ({handleSearch}) => {
       <div className="relative group">
         <input
           type="text"
-          onChange={handleSearch}
+          onChange={handleSearch}  // "scale-110 opacity-100" : "scale-90 opacity-0"
           placeholder="Search... 3 characters at least"
-          className={`${animationStep === 0 ? "px-10" : "px-20"} flex py-2  pl-3 pr-30 border-0 w-full rounded-full bg-slate-700 text-lime-300 focus:outline-none focus:ring-1 focus:ring-lime-400 transition-all duration-500`}
+          className={`${animationStep === 0 ? "px-10" : "px-20 "} flex py-2  pl-3 pr-30 border-0 w-full rounded-full bg-slate-700 text-lime-300 focus:outline-none focus:ring-1 focus:ring-lime-400 transition-all duration-500`}
         />
 
         {/* Search Icon */}
         {animationStep === 0 && (   
           <img                                              // absolute top-1/2 right-12 transform -translate-y-1/2 flex items-center gap-1 text-gray-400 text-sm bg-slate-800 px-2 py-0.5 rounded-lg cursor-pointer hover:text-lime-400 transition-all duration-500
-            className={`${animationStep === 0 ? "" : "hidden"} absolute top-1/2 right-2 transform -translate-y-1/2 w-5 h-5 cursor-pointer hover:bg-lime-500/50 rounded-lg opacity-100 transition-all duration-500`}
+            className={`${animationStep === 0 ? "scale-110 opacity-100" : "hidden scale-90 opacity-0"} absolute top-1/2 right-2 transform -translate-y-1/2 w-5 h-5 cursor-pointer hover:bg-lime-500/50 rounded-lg opacity-100 transition-all duration-500`}
             src={search} // Replace with your actual search icon path
             alt="Search icon"
           />
@@ -42,7 +42,7 @@ const AnimatedSearchBar = ({handleSearch}) => {
 
         {/* Cmd + K Shortcut */}
         {animationStep === 1 && (
-          <div className={`${animationStep === 1 ? "" : "hidden"} absolute top-1/2 right-2 transform -translate-y-1/2 flex items-center gap-1 text-gray-400 text-sm bg-slate-800 px-2 py-0.5 rounded-lg cursor-pointer hover:text-lime-400 transition-all duration-500`}>
+          <div className={`${animationStep === 1 ? "scale-110 opacity-100" : "hidden scale-90 opacity-0"} absolute top-1/2 right-2 transform -translate-y-1/2 flex items-center gap-1 text-gray-400 text-sm bg-slate-800 px-2 py-0.5 rounded-lg cursor-pointer hover:text-lime-400 transition-all duration-500`}>
             <span className="bg-gray-700 px-1 py-0.5 rounded text-xs font-semibold">
               ⌘
             </span>
@@ -54,7 +54,7 @@ const AnimatedSearchBar = ({handleSearch}) => {
 
         {/* Ctrl + K Shortcut */}
         {animationStep === 2 && (
-          <div className={`${animationStep === 2 ? "" : "hidden"} absolute top-1/2 right-2 transform -translate-y-1/2 flex items-center gap-1 text-gray-400 text-sm bg-slate-800 px-2 py-0.5 rounded-lg cursor-pointer hover:text-lime-400 transition-all duration-500`}>
+          <div className={`${animationStep === 2 ? "scale-130 opacity-90" : "hidden scale-90 opacity-0"} absolute top-1/2 right-2 transform -translate-y-1/2 flex items-center gap-1 text-gray-400 text-sm bg-slate-800 px-2 py-0.5 rounded-lg cursor-pointer hover:text-lime-400 transition-all duration-500`}>
             
             <span className="bg-gray-700 px-1 py-0.5 rounded text-xs font-semibold">
               Ctrl
